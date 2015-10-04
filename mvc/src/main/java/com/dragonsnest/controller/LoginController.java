71eb4dbaaeee0b82bea9fe3e46567af647767e8f
147631 <147631@IC0-W-N01663.ssg.global>
2015-10-05 08:40:07 +0900

    junit 제거

--- mvc/src/main/java/com/dragonsnest/controller/LoginController.java
+++ mvc/src/main/java/com/dragonsnest/controller/LoginController.java
@@ -10,7 +10,6 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.junit.runner.Request;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
